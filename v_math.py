33b79551abff8adfdc51c8e3236edf0db254bd0f
osskari <oskarjorgen@gmail.com>
2019-09-12 13:20:19 +0000

    death

--- v_math.py
+++ v_math.py
@@ -44,6 +44,9 @@ class Point:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
